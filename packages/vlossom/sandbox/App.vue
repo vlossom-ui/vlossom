e27fee8d8a721f6e98016b3b85fb285e3524f5cf
smithoo <smithoo7646@gmail.com>
2025-03-17 02:28:13 +0900

    WIP: init vlossom package

---
+++ packages/vlossom/sandbox/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return {};
+    },
+});
+</script>
