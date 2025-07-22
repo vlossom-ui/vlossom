402d9174cd2b3a2e61158432a32d08304e066291
smithoo <smithoo@pubg.com>
2025-07-22 10:21:04 +0900

    feat(visualizer): add visualizer

--- packages/vlossom/vite.config.ts
+++ packages/vlossom/vite.config.ts
@@ -1,12 +1,12 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
+    plugins: [vue(), vueDevTools(), visualizer({ filename: 'visualizer.html' })],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
