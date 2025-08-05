f1f81e313084a83d9ef2915e6373845abfd198a2
smithoo <smithoo@pubg.com>
2025-08-05 17:38:24 +0900

    add color-pallete

--- packages/vlossom/sandbox/sandbox.ts
+++ packages/vlossom/sandbox/sandbox.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createVlossom } from '@/framework';
 
+// this will be replaced with "import 'vlossom/styles'"
+import '@/styles/index.css';
+import '@/styles/index.scss';
+
+console.log('Hello Vlossom');
+
 const app = createApp(App);
 
 app.use(createVlossom());
--- packages/vlossom/src/main.ts
+++ packages/vlossom/src/main.ts
@@ -1,4 +1,5 @@
 import './styles/index.css';
+import './styles/index.scss';
 
 console.log('Hello Vlossom');
 
