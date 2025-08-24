c1f18178d37411969a0523c2922178414ef6ded5
smithoo <smithoo7646@gmail.com>
2025-08-25 02:06:40 +0900

    fix build

--- packages/vlossom/sandbox/sandbox.ts
+++ packages/vlossom/sandbox/sandbox.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createVlossom } from '@/framework';
+import { VlossomComponents } from '@/components/component-map';
 
 // this will be replaced with "import 'vlossom/styles'"
 import '@/styles/index.css';
@@ -10,6 +11,6 @@ console.log('Hello Vlossom');
 
 const app = createApp(App);
 
-app.use(createVlossom());
+app.use(createVlossom({ components: VlossomComponents }));
 
 app.mount('#app');
