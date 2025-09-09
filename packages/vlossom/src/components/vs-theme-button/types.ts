029775b69d8b6f7d7d84ab7de4fc67e691293f4b
sunio <sunio@pubg.com>
2025-09-09 10:52:31 +0900

    fix: 누락된 모듈 선언 영역 추가

--- packages/vlossom/src/components/vs-theme-button/types.ts
+++ packages/vlossom/src/components/vs-theme-button/types.ts
@@ -1,1 +1,11 @@
+import type VsThemeButton from './VsThemeButton.vue';
+
+declare module 'vue' {
+    interface GlobalComponents {
+        VsThemeButton: typeof VsThemeButton;
+    }
+}
+
+export type { VsThemeButton };
+
 export interface VsThemeButtonStyleSet {}
