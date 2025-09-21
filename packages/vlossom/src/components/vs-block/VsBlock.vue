a01812b97a1174b2ec65af28923d19a51db1ca09
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-09-21 22:25:01 +0900

    feat(VsBlock): add VsBlock component with styles and types

---
+++ packages/vlossom/src/components/vs-block/VsBlock.vue
@@ -0,0 +1,43 @@
+<template>
+    <vs-responsive :class="['vs-block', colorSchemeClass]" :style="styleSetVariables" :grid="grid" :width="width">
+        <div v-if="$slots['title']" class="vs-block-title">
+            <slot name="title" />
+        </div>
+        <div class="vs-block-content">
+            <slot />
+        </div>
+    </vs-responsive>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue';
+import { useColorScheme, useStyleSet } from '@/composables';
+import { getColorSchemeProps, getResponsiveProps, getStyleSetProps } from '@/props';
+import { VsComponent } from '@/declaration';
+import VsResponsive from '@/components/vs-responsive/VsResponsive.vue';
+import type { VsBlockStyleSet } from './types';
+
+const name = VsComponent.VsBlock;
+
+export default defineComponent({
+    name,
+    components: { VsResponsive },
+    props: {
+        ...getResponsiveProps(),
+        ...getColorSchemeProps(),
+        ...getStyleSetProps<VsBlockStyleSet>(),
+    },
+    setup(props) {
+        const { colorScheme, styleSet } = toRefs(props);
+        const { colorSchemeClass } = useColorScheme(name, colorScheme);
+        const { styleSetVariables } = useStyleSet<VsBlockStyleSet>(name, styleSet);
+
+        return {
+            colorSchemeClass,
+            styleSetVariables,
+        };
+    },
+});
+</script>
+
+<style lang="css" src="./VsBlock.css" />
---
+++ packages/vlossom/src/components/vs-block/types.ts
@@ -0,0 +1,17 @@
+import type VsBlock from './VsBlock.vue';
+import type { BoxStyleSet, TextStyleSet } from '@/declaration';
+
+declare module 'vue' {
+    interface GlobalComponents {
+        VsBlock: typeof VsBlock;
+    }
+}
+
+export type { VsBlock };
+
+type BlockStyleSet = Omit<BoxStyleSet, 'display' | 'opacity'> & TextStyleSet;
+
+export interface VsBlockStyleSet extends BlockStyleSet {
+    title?: Omit<BlockStyleSet, 'border' | 'borderRadius'> & { bottomBorder?: string };
+    boxShadow?: string;
+}
