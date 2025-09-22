e9cac5db691883461cd618241e3a9b6d1f53bb99
sunio <sunio@pubg.com>
2025-09-22 16:16:40 +0900

    fix(VsLabelValue): wrapper div padding 제거

--- packages/vlossom/src/components/vs-label-value/types.ts
+++ packages/vlossom/src/components/vs-label-value/types.ts
@@ -1,6 +1,6 @@
 import type { BoxStyleSet, FlexStyleSet, TextStyleSet } from '@/declaration';
 
-export interface VsLabelValueStyleSet extends Omit<BoxStyleSet, 'backgroundColor'> {
+export interface VsLabelValueStyleSet extends Omit<BoxStyleSet, 'backgroundColor' | 'padding'> {
     label?: BoxStyleSet & FlexStyleSet & TextStyleSet & { width?: string };
     value?: BoxStyleSet & FlexStyleSet & TextStyleSet & { width?: string };
 }
