33934f5e5d9fe00254d15fcdabe66e599003fdff
sunio <sunio@pubg.com>
2025-09-22 15:09:15 +0900

    fix(VsLabelValue): TextStyleSet 추가

--- packages/vlossom/src/components/vs-label-value/types.ts
+++ packages/vlossom/src/components/vs-label-value/types.ts
@@ -1,6 +1,6 @@
-import type { BoxStyleSet, FlexStyleSet } from '@/declaration';
+import type { BoxStyleSet, FlexStyleSet, TextStyleSet } from '@/declaration';
 
 export interface VsLabelValueStyleSet extends Omit<BoxStyleSet, 'backgroundColor'> {
-    label?: BoxStyleSet & FlexStyleSet & { width?: string };
-    value?: BoxStyleSet & FlexStyleSet & { width?: string };
+    label?: BoxStyleSet & FlexStyleSet & TextStyleSet & { width?: string };
+    value?: BoxStyleSet & FlexStyleSet & TextStyleSet & { width?: string };
 }
