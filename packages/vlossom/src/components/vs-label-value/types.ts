0bec608a511d34551e2ae508af8273868289c85c
sunio <sunio@pubg.com>
2025-09-22 14:34:28 +0900

    fix(VsLabelValue): 누락된 스타일 기본 initial 값 설정

--- packages/vlossom/src/components/vs-label-value/types.ts
+++ packages/vlossom/src/components/vs-label-value/types.ts
@@ -1,6 +1,6 @@
 import type { BoxStyleSet, FlexStyleSet } from '@/declaration';
 
-export interface VsLabelValueStyleSet extends BoxStyleSet {
+export interface VsLabelValueStyleSet extends Omit<BoxStyleSet, 'backgroundColor'> {
     label?: BoxStyleSet & FlexStyleSet & { width?: string };
     value?: BoxStyleSet & FlexStyleSet & { width?: string };
 }
