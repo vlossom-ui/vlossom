ed6ba369072b49b4cb5721df41def165326f0419
sunio <sunio@pubg.com>
2025-09-22 17:24:18 +0900

    feat(VsLabelValue): FlexStyleSet 제거 및 누락된 폰트 관련 스타일 추가

--- packages/vlossom/src/components/vs-label-value/types.ts
+++ packages/vlossom/src/components/vs-label-value/types.ts
@@ -1,10 +1,6 @@
 import type { BoxStyleSet, FlexStyleSet, TextStyleSet } from '@/declaration';
 
 export interface VsLabelValueStyleSet extends Omit<BoxStyleSet, 'backgroundColor' | 'padding'> {
-    label?: Omit<BoxStyleSet, 'border' | 'borderRadius' | 'opacity' | 'display'> &
-        FlexStyleSet &
-        TextStyleSet & { width?: string };
-    value?: Omit<BoxStyleSet, 'border' | 'borderRadius' | 'opacity' | 'display'> &
-        FlexStyleSet &
-        TextStyleSet & { width?: string };
+    label?: Omit<BoxStyleSet, 'border' | 'borderRadius' | 'opacity' | 'display'> & TextStyleSet & { width?: string };
+    value?: Omit<BoxStyleSet, 'border' | 'borderRadius' | 'opacity' | 'display'> & TextStyleSet & { width?: string };
 }
