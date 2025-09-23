3102603297c032c9fcfca97d6ee71577ba40c8e2
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-09-23 18:12:08 +0900

    feat(VsProgress): progress 컴포넌트의 value와 max 속성을 문자열과 숫자 모두 지원하도록 변경

--- packages/vlossom/src/components/vs-progress/VsProgress.vue
+++ packages/vlossom/src/components/vs-progress/VsProgress.vue
@@ -1,9 +1,14 @@
 <template>
-    <progress :value="value" :max="max" :class="['vs-progress', colorSchemeClass]" :style="componentStyleSet" />
+    <progress
+        :class="['vs-progress', colorSchemeClass]"
+        :style="componentStyleSet"
+        :value="computedValue"
+        :max="computedMax"
+    />
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from 'vue';
+import { defineComponent, toRefs, computed } from 'vue';
 import { useColorScheme, useStyleSet } from '@/composables';
 import { VsComponent } from '@/declaration';
 import { getColorSchemeProps, getStyleSetProps } from '@/props';
@@ -15,17 +20,28 @@ export default defineComponent({
     props: {
         ...getColorSchemeProps(),
         ...getStyleSetProps<VsProgressStyleSet>(),
-        max: { type: Number, default: 100 },
-        value: { type: Number, default: 0 },
+        max: { type: [Number, String], default: 100 },
+        value: { type: [Number, String], default: 0 },
     },
     setup(props) {
         const { colorScheme, styleSet } = toRefs(props);
         const { colorSchemeClass } = useColorScheme(name, colorScheme);
         const { componentStyleSet } = useStyleSet<VsProgressStyleSet>(name, styleSet);
+        const { value, max } = toRefs(props);
+
+        const computedValue = computed(() => {
+            return Number(value.value);
+        });
+
+        const computedMax = computed(() => {
+            return Number(max.value);
+        });
 
         return {
             colorSchemeClass,
             componentStyleSet,
+            computedValue,
+            computedMax,
         };
     },
 });
