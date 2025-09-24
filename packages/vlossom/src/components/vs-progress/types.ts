807f0379c6e08f36aab740ba087eea13889beba3
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-09-24 13:11:33 +0900

    feat(VsProgress): 레이블 스타일 추가 및 CSS 개선

--- packages/vlossom/src/components/vs-progress/types.ts
+++ packages/vlossom/src/components/vs-progress/types.ts
@@ -1,5 +1,5 @@
 import type VsProgress from './VsProgress.vue';
-import type { SizeStyleSet, BoxStyleSet } from '@/declaration';
+import type { SizeStyleSet, BoxStyleSet, TextStyleSet } from '@/declaration';
 
 declare module 'vue' {
     interface GlobalComponents {
@@ -11,4 +11,5 @@ export type { VsProgress };
 
 export interface VsProgressStyleSet extends SizeStyleSet, Omit<BoxStyleSet, 'display' | 'opacity' | 'padding'> {
     valueColor?: string;
+    label?: TextStyleSet & { textShadow?: string };
 }
