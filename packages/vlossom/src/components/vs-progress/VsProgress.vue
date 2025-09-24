ded4ac5f1c7e0df05667c913b79cb26bdbfa174b
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-09-24 16:46:20 +0900

    fix(VsProgress): isNaN 대신 isFinite 사용하도록 수정

--- packages/vlossom/src/components/vs-progress/VsProgress.vue
+++ packages/vlossom/src/components/vs-progress/VsProgress.vue
@@ -26,7 +26,7 @@ export default defineComponent({
             default: 100,
             validator: (value: number | string) => {
                 const num = Number(value);
-                return !isNaN(num) && num >= 0;
+                return !isFinite(num) && num >= 0;
             },
         },
         value: {
@@ -34,7 +34,7 @@ export default defineComponent({
             default: 0,
             validator: (value: number | string) => {
                 const num = Number(value);
-                return !isNaN(num) && num >= 0;
+                return !isFinite(num) && num >= 0;
             },
         },
         label: { type: String, default: '' },
@@ -47,7 +47,7 @@ export default defineComponent({
 
         const computedMax = computed(() => {
             const num = Number(max.value);
-            if (isNaN(num) || num < 0) {
+            if (isFinite(num) || num < 0) {
                 return 0;
             }
 
@@ -56,7 +56,7 @@ export default defineComponent({
 
         const computedValue = computed(() => {
             const num = Number(value.value);
-            if (isNaN(num) || num < 0) {
+            if (isFinite(num) || num < 0) {
                 return 0;
             }
             if (num > computedMax.value) {
