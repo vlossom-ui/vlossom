f81232e2782dce4c222a12ff5d13934e37cad157
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-10-21 18:20:18 +0900

    feat(props-util): add props utility functions for property validation

--- packages/vlossom/src/utils/index.ts
+++ packages/vlossom/src/utils/index.ts
@@ -3,3 +3,4 @@ export * from './function-util';
 export * from './log-util';
 export * from './object-util';
 export * from './string-util';
+export * from './props-util';
---
+++ packages/vlossom/src/utils/props-util.ts
@@ -0,0 +1,19 @@
+import { logUtil } from './log-util';
+
+export const propsUtil = {
+    checkPropExist<T = any>(componentName: string, property: string, options: any, value: T) {
+        if (!options.includes(value)) {
+            logUtil.propError(componentName, property, `${property} must be one of ${options.join(', ')}`);
+            return false;
+        }
+        return true;
+    },
+    checkValidNumber(componentName: string, property: string, value: number | string) {
+        const num = Number(value);
+        const isValid = !isNaN(num) && num >= Number.MIN_SAFE_INTEGER && num <= Number.MAX_SAFE_INTEGER;
+        if (!isValid) {
+            logUtil.propError(componentName, property, `invalid ${property} value`);
+        }
+        return isValid;
+    },
+};
