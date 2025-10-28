7edb8dad0a351a64ac7c76436b465156be3d891d
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-10-28 14:01:19 +0900

    refactor(props-util): remove unused checkPropExist function

--- packages/vlossom/src/utils/props-util.ts
+++ packages/vlossom/src/utils/props-util.ts
@@ -1,13 +1,6 @@
 import { logUtil } from './log-util';
 
 export const propsUtil = {
-    checkPropExist<T = any>(componentName: string, property: string, options: any, value: T) {
-        if (!options.includes(value)) {
-            logUtil.propError(componentName, property, `${property} must be one of ${options.join(', ')}`);
-            return false;
-        }
-        return true;
-    },
     checkValidNumber(componentName: string, property: string, value: number | string) {
         const num = Number(value);
         const isValid = !isNaN(num) && num >= Number.MIN_SAFE_INTEGER && num <= Number.MAX_SAFE_INTEGER;
