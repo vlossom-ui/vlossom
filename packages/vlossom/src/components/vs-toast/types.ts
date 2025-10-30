41836ab8b0279a9f01f638b9f7145d940d93d4dd
smithoo <smithoo@pubg.com>
2025-10-30 15:28:50 +0900

    fix export component types

--- packages/vlossom/src/components/vs-toast/types.ts
+++ packages/vlossom/src/components/vs-toast/types.ts
@@ -1,13 +1,15 @@
 import type { BoxStyleSet } from '@/declaration';
 import type VsToast from './VsToast.vue';
+import type VsToastView from './VsToastView.vue';
 
 declare module 'vue' {
     interface GlobalComponents {
         VsToast: typeof VsToast;
+        VsToastView: typeof VsToastView;
     }
 }
 
-export type { VsToast };
+export type { VsToast, VsToastView };
 
 export interface VsToastStyleSet extends BoxStyleSet {
     height?: string;
