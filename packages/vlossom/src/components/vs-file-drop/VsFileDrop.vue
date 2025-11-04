e943c675d7b656b54c3630d18ea3b61ab34b9906
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-11-04 18:05:14 +0900

    feat(VsFileDrop): implement file drop component with drag-and-drop support, styling, and file handling functionality

---
+++ packages/vlossom/src/components/vs-file-drop/VsFileDrop.vue
@@ -0,0 +1,277 @@
+<template>
+    <vs-input-wrapper
+        v-show="!hidden"
+        :width
+        :grid
+        :disabled
+        :hidden
+        :id="computedId"
+        :label
+        :no-label
+        :no-messages
+        :required
+        :small
+        :messages="computedMessages"
+        :shake
+    >
+        <template #label v-if="!noLabel && (!!label || !!$slots.label)">
+            <slot name="label" />
+        </template>
+
+        <div
+            :class="['vs-file-drop', colorSchemeClass, classObj, stateClasses]"
+            :style="styleSetVariables"
+            @drop.prevent.stop="handleFileDrop"
+            @dragenter.prevent.stop="setDragging(true)"
+            @dragover.prevent.stop="setDragging(true)"
+            @dragleave.prevent.stop="setDragging(false)"
+        >
+            <input
+                ref="fileDropRef"
+                type="file"
+                class="vs-file-drop-ref"
+                :id="computedId"
+                :name="name"
+                :disabled="computedDisabled"
+                :readonly="computedReadonly"
+                :aria-required="required"
+                :accept="accept"
+                @change.stop="handleFileDialog"
+                @keydown.enter.stop="openFileDialog"
+                @keydown.space.prevent.stop="openFileDialog"
+            />
+
+            <div class="vs-file-drop-content">
+                <slot :dragging="dragging">
+                    <div v-if="hasValue" class="vs-file-drop-files">
+                        <div
+                            v-for="(file, index) in computedInputValue"
+                            :key="`${file.name}-${index}`"
+                            class="vs-file-drop-file"
+                        >
+                            <vs-chip
+                                :id="file.name"
+                                :small
+                                :color-scheme="colorScheme"
+                                :closable="!computedDisabled"
+                                @close="handleFileRemove(file)"
+                            >
+                                <span class="vs-file-drop-file-name">{{ file.name }}</span>
+                                <span class="vs-file-drop-file-size">{{
+                                    `(${stringUtil.toFileSizeFormat(file.size)})`
+                                }}</span>
+                            </vs-chip>
+                        </div>
+                    </div>
+
+                    <div v-else class="vs-file-drop-placeholder">
+                        <i
+                            class="placeholder-icon"
+                            :class="{ 'size-4': small, 'size-6': !small }"
+                            v-html="attachFileIcon"
+                        />
+                        <span class="placeholder-text">{{ placeholder }}</span>
+                    </div>
+                </slot>
+            </div>
+        </div>
+
+        <template #messages v-if="!noMessages">
+            <slot name="messages" />
+        </template>
+    </vs-input-wrapper>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref, toRefs, useTemplateRef, type PropType, type Ref, type TemplateRef } from 'vue';
+import { VsComponent } from '@/declaration';
+import { useColorScheme, useStyleSet, useInput, useStateClass } from '@/composables';
+import { getInputProps, getResponsiveProps, getColorSchemeProps, getStyleSetProps } from '@/props';
+import type { FileDropValueType, VsFileDropStyleSet } from './types';
+import { useVsFileDropRules } from './vs-file-drop-rules';
+import { stringUtil, propsUtil } from '@/utils';
+import { attachFileIcon } from './icons';
+import VsInputWrapper from '@/components/vs-input-wrapper/VsInputWrapper.vue';
+import VsChip from '@/components/vs-chip/VsChip.vue';
+
+const name = VsComponent.VsFileDrop;
+
+export default defineComponent({
+    name,
+    components: { VsInputWrapper, VsChip },
+    props: {
+        ...getInputProps<FileDropValueType>(),
+        ...getResponsiveProps(),
+        ...getColorSchemeProps(),
+        ...getStyleSetProps<VsFileDropStyleSet>(),
+        accept: { type: String, default: '' },
+        max: {
+            type: [Number, String],
+            default: Number.MAX_SAFE_INTEGER,
+            validator: (value: number | string) => propsUtil.checkValidNumber(name, 'max', value),
+        },
+        min: {
+            type: [Number, String],
+            default: Number.MIN_SAFE_INTEGER,
+            validator: (value: number | string) => propsUtil.checkValidNumber(name, 'min', value),
+        },
+        // v-model
+        modelValue: {
+            type: [Object, Array] as PropType<FileDropValueType>,
+            default: null,
+        },
+    },
+    emits: ['update:modelValue', 'update:changed', 'update:valid', 'change', 'drop'],
+    setup(props, { emit }) {
+        const {
+            colorScheme,
+            small,
+            styleSet,
+            required,
+            accept,
+            modelValue,
+            id,
+            disabled,
+            readonly,
+            messages,
+            rules,
+            state,
+            max,
+            min,
+        } = toRefs(props);
+
+        const inputValue: Ref<FileDropValueType> = ref(modelValue.value);
+        const fileDropRef: TemplateRef<HTMLInputElement | null> = useTemplateRef('inputRef');
+        const dragging = ref(false);
+
+        const { colorSchemeClass } = useColorScheme(name, colorScheme);
+        const { styleSetVariables } = useStyleSet<VsFileDropStyleSet>(name, styleSet);
+        const { requiredCheck, maxCheck, minCheck, acceptCheck } = useVsFileDropRules(required, max, min, accept);
+
+        const { computedId, computedMessages, computedState, computedDisabled, computedReadonly, shake, validate } =
+            useInput(
+                { emit },
+                {
+                    inputValue,
+                    modelValue,
+                    id,
+                    disabled,
+                    readonly,
+                    messages,
+                    rules,
+                    defaultRules: computed(() => [requiredCheck, maxCheck, minCheck, acceptCheck]),
+                    state,
+                    callbacks: {
+                        onMounted: () => {
+                            inputValue.value = modelValue.value;
+                        },
+                    },
+                },
+            );
+
+        const classObj = computed(() => ({
+            'vs-small': small.value,
+            'vs-focusable': !computedDisabled.value && !computedReadonly.value,
+            'vs-disabled': computedDisabled.value,
+            'vs-readonly': computedReadonly.value,
+            'vs-dragging': dragging.value,
+        }));
+
+        const { stateClasses } = useStateClass(computedState);
+        const computedInputValue = computed<File[]>(() => {
+            return inputValue.value ?? [];
+        });
+        const hasValue = computed(() => computedInputValue.value.length > 0);
+
+        function setDragging(value: boolean) {
+            if (computedDisabled.value || computedReadonly.value) {
+                return;
+            }
+
+            if (dragging.value === value) {
+                return;
+            }
+
+            dragging.value = value;
+        }
+
+        function openFileDialog() {
+            if (computedReadonly.value || computedDisabled.value) {
+                return;
+            }
+
+            fileDropRef.value?.click();
+        }
+
+        function setInputValue(files: File[]): void {
+            if (!files || files.length === 0) {
+                return;
+            }
+
+            inputValue.value = files;
+        }
+
+        function handleFileDialog(event: Event): void {
+            const target = event.target as HTMLInputElement;
+            const files = Array.from(target.files || []);
+
+            if (files.length === 0) {
+                return;
+            }
+
+            setInputValue(files);
+            emit('update:changed', files);
+        }
+
+        function handleFileDrop(event: DragEvent): void {
+            const files = Array.from(event.dataTransfer?.files || []);
+
+            emit('drop', files);
+            setDragging(false);
+
+            if (computedDisabled.value || computedReadonly.value) {
+                return;
+            }
+
+            setInputValue(files);
+        }
+
+        function handleFileRemove(target: File): void {
+            if (!target || !inputValue.value) {
+                return;
+            }
+
+            const files = computedInputValue.value;
+            const filteredFiles = files.filter((file) => file !== target);
+
+            inputValue.value = filteredFiles;
+        }
+
+        return {
+            fileDropRef,
+            computedId,
+            computedMessages,
+            computedDisabled,
+            computedReadonly,
+            shake,
+            colorSchemeClass,
+            styleSetVariables,
+            classObj,
+            stateClasses,
+            dragging,
+            computedInputValue,
+            hasValue,
+            stringUtil,
+            attachFileIcon,
+            validate,
+            setDragging,
+            openFileDialog,
+            handleFileDialog,
+            handleFileDrop,
+            handleFileRemove,
+        };
+    },
+});
+</script>
+
+<style src="./VsFileDrop.css"></style>
