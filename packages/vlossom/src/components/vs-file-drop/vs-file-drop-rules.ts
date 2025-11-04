8b68af1da111be6cdfb3445f5250566f3fe4350a
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-11-04 18:04:51 +0900

    feat(VsFileDropRules): implement validation rules for file drop component

---
+++ packages/vlossom/src/components/vs-file-drop/vs-file-drop-rules.ts
@@ -0,0 +1,57 @@
+import { type Ref } from 'vue';
+import type { FileDropValueType } from './types';
+
+export function useVsFileDropRules(
+    required: Ref<boolean>,
+    max: Ref<number | string>,
+    min: Ref<number | string>,
+    accept: Ref<string>,
+) {
+    function requiredCheck(v: FileDropValueType): string {
+        if (required.value && (v === null || v.length === 0)) {
+            return 'required';
+        }
+
+        return '';
+    }
+
+    function maxCheck(v: FileDropValueType): string {
+        const limit = Number(max.value);
+        if (v && v.length > limit) {
+            return `You can only upload up to ${max.value} files`;
+        }
+
+        return '';
+    }
+
+    function minCheck(v: FileDropValueType): string {
+        const limit = Number(min.value);
+        if (v && v.length < limit) {
+            return `You must upload at least ${min.value} files`;
+        }
+
+        return '';
+    }
+
+    function acceptCheck(v: FileDropValueType): string {
+        if (accept.value && v && v.length > 0) {
+            const acceptedTypes = accept.value.split(',').map((type) => type.trim());
+            const files = v.map((file) => file.type);
+            const invalidFiles = files.filter((file) => !acceptedTypes.includes(file));
+            if (invalidFiles.length > 0) {
+                return `You can only upload files with the following extensions: ${acceptedTypes.join(', ')}`;
+            }
+        }
+
+        return '';
+    }
+
+    // 파일 타입도 체크
+
+    return {
+        requiredCheck,
+        maxCheck,
+        minCheck,
+        acceptCheck,
+    };
+}
