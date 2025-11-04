b2d32543dd788775ee6eaaa90b2ac797d8ec9bd1
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-11-04 18:05:00 +0900

    feat(string-util): add toFileSizeFormat utility function for converting bytes to human-readable file sizes

--- packages/vlossom/src/utils/string-util.ts
+++ packages/vlossom/src/utils/string-util.ts
@@ -17,5 +17,16 @@ export const stringUtil = {
         }
         return stringValue;
     },
+    toFileSizeFormat(bytes: number): string {
+        if (bytes === 0) {
+            return '0 Bytes';
+        }
+
+        const k = 1024;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return `${Math.round((bytes / Math.pow(k, i)) * 100) / 100} ${sizes[i]}`;
+    },
     kebabCase,
 };
