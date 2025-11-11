869aa52e99fc75b2928bcc5a0f0a4462a0fa1878
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-11-11 15:29:14 +0900

    refactor(App.vue): remove vs-file-drop component from sandbox template

--- packages/vlossom/sandbox/App.vue
+++ packages/vlossom/sandbox/App.vue
@@ -2,12 +2,6 @@
     <div>
         <h1>Hello Vlossom</h1>
     </div>
-    <vs-file-drop
-        placeholder="파일을 드래그하거나 클릭하여 업로드하세요"
-        multiple
-        class="mx-auto mt-100"
-        width="500px"
-    />
 </template>
 
 <script lang="ts">
