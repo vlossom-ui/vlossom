b77ae505ad5170d1f31b187e703ea8f6801f75b3
Jungwon Bae (배정원) <bjw0111@pubg.com>
2025-11-26 11:57:41 +0900

    fix(VsTabs): update SVG icons to use currentColor and adjust tab indicator styles

--- packages/vlossom/src/components/vs-tabs/icons.ts
+++ packages/vlossom/src/components/vs-tabs/icons.ts
@@ -2,11 +2,11 @@ export const vsTabsIcons = {
     // chevron_right
     goNext: `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-            <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
+            <path fill="currentColor" d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
         </svg>`,
     // chevron_left
     goPrev: `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-            <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
+            <path fill="currentColor" d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
         </svg>`,
 };
