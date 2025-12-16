95c798c8694a95f79cc5dc219bc50f9397311318
smithoo <smithoo@pubg.com>
2025-12-16 12:57:06 +0900

    restore App.vue

--- packages/vlossom/sandbox/App.vue
+++ packages/vlossom/sandbox/App.vue
@@ -1,28 +1,16 @@
 <template>
     <div>
         <h1>Hello Vlossom</h1>
-
-        <vs-button @click="isOpen = !isOpen">Toggle Modal</vs-button>
-
-        <vs-modal v-model="isOpen" focus-lock>
-            <modal-content />
-        </vs-modal>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import ModalContent from './ModalContent.vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'App',
-    components: { ModalContent },
     setup() {
-        const isOpen = ref(false);
-
-        return {
-            isOpen,
-        };
+        return {};
     },
 });
 </script>
