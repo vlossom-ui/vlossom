0a91b94d9c10c9676fa5fe23107e8adfbf0feacd
Jungwon Bae (배정원) <bjw0111@pubg.com>
2026-01-07 18:57:14 +0900

    chore(storybook): add @storybook/addon-vitest to Storybook configuration and clean up viteFinal plugins

--- packages/vlossom/.storybook/main.ts
+++ packages/vlossom/.storybook/main.ts
@@ -3,13 +3,13 @@ import { withoutVitePlugins } from '@storybook/builder-vite';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-a11y'],
+    addons: ['@chromatic-com/storybook', '@storybook/addon-docs', '@storybook/addon-a11y', '@storybook/addon-vitest'],
     framework: {
         name: '@storybook/vue3-vite',
         options: {},
     },
     async viteFinal(c) {
-        c.plugins = await withoutVitePlugins(c.plugins, ['vite:dts', 'vite-plugin-inspect']);
+        c.plugins = await withoutVitePlugins(c.plugins, ['vite:dts']);
         return c;
     },
 };
