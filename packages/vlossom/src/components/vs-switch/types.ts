d8d5abc7cb0f9c257d043e305487ef4b8ec32ade
Jungwon Bae (배정원) <bjw0111@pubg.com>
2026-01-30 18:20:50 +0900

    refactor(vs-switch): add CSSProperties type to VsSwitchStyleSet for enhanced styling flexibility

--- packages/vlossom/src/components/vs-switch/types.ts
+++ packages/vlossom/src/components/vs-switch/types.ts
@@ -1,4 +1,4 @@
-import type { ComponentPublicInstance } from 'vue';
+import type { ComponentPublicInstance, CSSProperties } from 'vue';
 import type { FocusableRef, FormChildRef } from '@/declaration';
 import type { VsInputWrapperStyleSet } from '@/components/vs-input-wrapper/types';
 import type VsSwitch from './VsSwitch.vue';
@@ -15,14 +15,9 @@ export interface VsSwitchRef extends ComponentPublicInstance<typeof VsSwitch>, F
 
 export interface VsSwitchStyleSet {
     variables?: {
-        width?: string;
-        height?: string;
-        backgroundColor?: string;
-        border?: string;
-        borderRadius?: string;
-        fontColor?: string;
         handleColor?: string;
         handleSize?: string;
     };
+    component?: CSSProperties;
     wrapper?: VsInputWrapperStyleSet;
 }
