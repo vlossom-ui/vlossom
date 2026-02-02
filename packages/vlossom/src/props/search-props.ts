be16ba76b761dabafd02824e6c81d0e4c8bbf6ae
sunio <sunio@pubg.com>
2026-02-02 17:28:24 +0900

    feat(search-props): add validator for search prop to handle boolean and object types

--- packages/vlossom/src/props/search-props.ts
+++ packages/vlossom/src/props/search-props.ts
@@ -1,11 +1,18 @@
 import type { PropType } from 'vue';
 import type { SearchProps } from '@/declaration';
+import { objectUtil } from '@/utils';
 
 export function getSearchProps() {
     return {
         search: {
             type: [Boolean, Object] as PropType<SearchProps>,
             default: false,
+            validator: (value: SearchProps) => {
+                if (typeof value === 'boolean') {
+                    return true;
+                }
+                return objectUtil.isEmpty(value);
+            },
         },
     };
 }
