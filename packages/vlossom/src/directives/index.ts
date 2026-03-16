aceee31eae422d6c8580690b438a76b7df23b98e
sunio <sunio@pubg.com>
2026-03-16 16:55:58 +0900

    feat(directives): add v-scroll-shadow directive for scroll position shadows

---
+++ packages/vlossom/src/directives/index.ts
@@ -0,0 +1,1 @@
+export * from './vs-scroll-shadow/scroll-shadow';
---
+++ packages/vlossom/src/directives/vs-scroll-shadow/scroll-shadow.ts
@@ -0,0 +1,76 @@
+import type { Directive } from 'vue';
+import { logUtil, domUtil } from '@/utils';
+
+const SCROLL_SHADOW_MIXIN_CLASS = 'vs-scroll-shadow';
+const SCROLL_STATE_VALUE = 'scroll-state';
+
+// global state
+const originalContainerTypeMap = new WeakMap<HTMLElement, string>();
+
+function verifyScrollContainer(el: HTMLElement): void {
+    const isScrollable = domUtil.isScrollableX(el) || domUtil.isScrollableY(el);
+    if (!isScrollable) {
+        logUtil.warning(
+            'v-scroll-shadow',
+            `Element is not a scrollable container. "overflow" must be "auto" or "scroll" ${el}`,
+        );
+    }
+}
+
+function activate(el: HTMLElement): void {
+    const originalContainerType = el.style.containerType;
+    const computedStyle = getComputedStyle(el).getPropertyValue('container-type').trim();
+
+    originalContainerTypeMap.set(el, originalContainerType);
+    /**
+     * @note 'scroll-state' is 반드시 needed for scroll shadow to work.
+     * some elements may have already set a other container type, so we need to add 'scroll-state' to the existing container type.
+     *
+     * e.g. container-type: inline-size; -> container-type: inline-size scroll-state;
+     */
+    if (!computedStyle.includes(SCROLL_STATE_VALUE)) {
+        const activatedContainerType =
+            computedStyle && computedStyle !== 'normal' ? `${computedStyle} ${SCROLL_STATE_VALUE}` : SCROLL_STATE_VALUE;
+        el.style.containerType = activatedContainerType;
+    }
+
+    el.classList.add(SCROLL_SHADOW_MIXIN_CLASS);
+}
+
+function deactivate(el: HTMLElement): void {
+    el.classList.remove(SCROLL_SHADOW_MIXIN_CLASS);
+
+    const originalContainerType = originalContainerTypeMap.get(el);
+    if (originalContainerType) {
+        el.style.containerType = originalContainerType;
+    } else {
+        el.style.removeProperty('container-type');
+    }
+    originalContainerTypeMap.delete(el);
+}
+
+export const scrollShadow: Directive<HTMLElement, boolean | undefined> = {
+    mounted(el, binding) {
+        if (binding.value === false) {
+            return;
+        }
+        verifyScrollContainer(el);
+        activate(el);
+    },
+
+    updated(el, binding) {
+        if (binding.value === binding.oldValue) {
+            return;
+        }
+        if (binding.value === false) {
+            deactivate(el);
+        } else {
+            verifyScrollContainer(el);
+            activate(el);
+        }
+    },
+
+    unmounted(el) {
+        deactivate(el);
+    },
+};
