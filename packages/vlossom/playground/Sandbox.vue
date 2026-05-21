0037b03d8051112838cdef97a66ef2bf1ae3e010
smithoo <smithoo@pubg.com>
2026-05-22 02:33:15 +0900

    update Sandbox

--- packages/vlossom/playground/Sandbox.vue
+++ packages/vlossom/playground/Sandbox.vue
@@ -40,11 +40,23 @@
                 </vs-label-value>
             </section>
 
+            <h1 class="mt-8 text-2xl font-bold">VsLabelValue sizes (vertical)</h1>
+            <section v-for="size in sizes" :key="`label-value-vertical-${size}`" class="flex flex-col gap-2">
+                <h2 class="text-lg font-semibold">size = "{{ size }}"</h2>
+                <vs-label-value :size="size" vertical>
+                    <template #label>Label</template>
+                    Lorem ipsum dolor sit amet
+                </vs-label-value>
+            </section>
+
             <h1 class="mt-8 text-2xl font-bold">VsAccordion sizes</h1>
             <section v-for="size in sizes" :key="`accordion-${size}`" class="flex flex-col gap-2">
                 <h2 class="text-lg font-semibold">size = "{{ size }}"</h2>
                 <vs-accordion :size="size">
                     <template #title>size = "{{ size }}"</template>
+                    <p>아코디언 내용입니다.</p>
+                    <p>아코디언 내용입니다.</p>
+                    <p>아코디언 내용입니다.</p>
                     <p>아코디언 내용입니다.</p>
                 </vs-accordion>
             </section>
