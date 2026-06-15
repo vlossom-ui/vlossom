e69ac6c1ce617ffbe03a53f60613c351dab71b5b
Jungwon Bae (배정원) <bjw0111@pubg.com>
2026-06-15 12:37:58 +0900

    fix(VsProgress): add validation logging for max value and current value inputs

--- packages/vlossom/src/components/vs-progress/VsProgress.vue
+++ packages/vlossom/src/components/vs-progress/VsProgress.vue
@@ -27,6 +27,11 @@ export default defineComponent({
             default: 1,
             validator: (value: number | string) => {
                 const num = Number(value);
+
+                if (!isFinite(num) || num <= 0) {
+                    logUtil.warning(componentName, 'Max value should be a finite number and greater than 0');
+                }
+
                 return isFinite(num) && num > 0;
             },
         },
@@ -35,6 +40,11 @@ export default defineComponent({
             default: 0,
             validator: (value: number | string) => {
                 const num = Number(value);
+
+                if (!isFinite(num) || num < 0) {
+                    logUtil.warning(componentName, 'Value should be a finite number and greater than or equal to 0');
+                }
+
                 return isFinite(num) && num >= 0;
             },
         },
@@ -52,23 +62,11 @@ export default defineComponent({
         const computedMax = computed(() => {
             const num = Number(max.value);
 
-            if (isNaN(num)) {
-                logUtil.warning(componentName, 'Max value should be a number');
-            }
-
             return numberUtil.clamp(num, 1, Number.MAX_SAFE_INTEGER);
         });
 
         const computedValue = computed(() => {
             const num = Number(value.value);
-
-            if (isNaN(num)) {
-                logUtil.warning(componentName, 'Value should be a number');
-            }
-
-            if (num > computedMax.value || num < 0) {
-                logUtil.warning(componentName, `Value should be in the range of 0 to ${computedMax.value}`);
-            }
 
             return numberUtil.clamp(num, 0, computedMax.value);
         });
